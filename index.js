a616ca350dfcd3fe5910e8d932c3d4f20d7d457f
Justina Wong <justinawong97@gmail.com>
2023-04-27 22:26:02 -0700

    v2 partial

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const Leaderboard = require('./models/leaderboard');
 const uri = process.env.URI;
 const app = express();
 const port = 8080;
+const alphabetLower = "abcdefghijklmnopqrstuvwxyz";
 
 // an array just to manipulate the json data from the file,
 // will be removed when db is added.
@@ -89,7 +90,7 @@ app.post('/newplayer', async (req, res) => {
     "current_word": "",   // the "current" word that the user is guessing.
     "guessing_word": "",  // the word that the user is guessing, but would be exatly opposite of the user's guess. (explained below)
     "words_left": ["hello", "miserable", "ecstatic","bliss","color","heaven","mountain","sky","riddle","library"],
-    "submitted_date": Date.now,
+    "submitted_date": Math.floor(Date.now() / 1000), // Time in seconds UTC - this needs to be converted on the FE
     "guesses_left": 8,
     "hints_left": 3
   };
@@ -179,6 +180,97 @@ app.get('/make-guess/:user_id/:guess', async (req, res) => {
 
 });
 
+
+app.get('/make-guess/v2/:user_id/:guess', async (req, res) => {
+  userID = req.params.user_id;
+  guess = req.params.guess.toLowerCase(); // lowercase letters only
+  if (guess.length !== 1) {
+    // should be a single letter
+    res.status(400).send([]) // 400 bad request
+    return
+  }
+  if (!alphabetLower.includes(guess)) {
+    // guess not a letter
+    res.status(400).send([]) // 400 bad request
+    return
+  }
+  const data_file = fs.readFileSync('./leaderboard.json', 'utf-8');
+  const data_json = JSON.parse(data_file);
+  
+  var entry = data_json.find(entry => entry.user_id == userID);
+  var index = data_json.indexOf(entry)
+  console.log(index)
+  if (entry.guesses_left <= 0) {
+    // no more guesses left
+    res.status(200).json({"winner": false});
+    return
+  }
+
+  var word = entry.current_word
+
+  var guessedLetters = entry.guessing_word
+  console.log(guessedLetters)
+  if (guessedLetters.includes(guess)) {
+    console.log("already guessed")
+    res.status(400).send([]) // 400 bad request
+    return
+  }
+
+  // guessing_word is the correct guesses
+  if (word.includes(guess)) {
+    // Word contains the guess, do not dock guess and renew 'guessing_word'
+    var guessingWord = data_json[index].guessing_word
+
+    // TODO: need guessed_letters in the database
+    // This will make it easy also to keep track of already guessed letters e.g. both correct/incorrect guesses to be disabled on the frontend
+    var guessedLetters = guessingWord.split("").filter((e) => { return alphabetLower.includes(e)}) // only letters from guessing_word
+    console.log("Guessed letters:", guessedLetters)
+
+    guessedLetters.push(guess)
+    var newGuessingWord = ""
+    for (let i = 0; i < word.length; i++) {
+      if (guessedLetters.includes(word[i])) {
+        newGuessingWord += word[i];
+      } else {
+        newGuessingWord += "_";
+      }
+    }
+
+    console.log("newGuessingWord is", newGuessingWord)
+    data_json[index].guessing_word = newGuessingWord
+
+    if (!newGuessingWord.includes("_")) {
+      // new word: add word to guessed word, remove from words left, set new word
+      data_json[index].words_guessed.push(word)
+  
+      data_json[index].words_left = data_json[index].words_left.filter((e) => {
+        return e !== word
+      })
+  
+      const randomIndex = Math.floor(Math.random() * data_json[index].words_left.length);
+  
+      if (data_json[index].words_left.length > 0) {
+        data_json[index].current_word = data_json[index].words_left[randomIndex];
+        res.status(200).json({"next_word": data_json[index].current_word});
+      }
+      else {
+        data_json[index].current_word = ""
+        res.status(200).json({"winner": true});
+      }
+    }
+
+  } else {
+    // Word does not contain the guess, dock point from 'guesses_left'
+    data_json[index].guesses_left -= 1
+  }
+
+  fs.writeFileSync('./leaderboard.json', JSON.stringify(data_json));
+  if (data_json[index].guesses_left === 0) {
+    res.status(200).json({"winner": false});
+  }else {
+    res.status(200).json({"word_so_far": data_json[index].guessing_word, "guesses_left": data_json[index].guesses_left});
+  }
+});
 
 
 
